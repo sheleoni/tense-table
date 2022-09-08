33e082170d9c7af435e43fdffe5690955b8024ee
Shelly <shelly@Shellys-MacBook-Air.local>
2022-09-08 09:36:33 +0900

    feat: added comments to indicate tenses and their sections

--- src/script.js
+++ src/script.js
@@ -11,16 +11,15 @@ After console.log("simple"), write the string "simple" in <em class="selected-te
 
 */
 
-
 // select table headings
 
- // in the category of "tenses":
-const simple = $( ".simple" );
+// in the category of "tenses":
+const simple = $(".simple");
 const continuous = $(".continuous");
 const perfect = $(".perfect");
 const perfectContinuous = $(".perfect-continuous");
 
- // in the category of "time":
+// in the category of "time":
 const past = $(".past");
 const present = $(".present");
 const future = $(".future");
@@ -32,41 +31,44 @@ let selectedTense = $(".selected-tense");
 
 // create functions that will display the clicked table headings on the display area.
 
-    // table headings that will be placed in the "tenses" placeholder
-    simple.click(function() { // onClick of the "simple" in the table heading
-        selectedTense.html(`<em class="selected-tense clicked">Simple</em>`);
-        console.log("Simple selected!");
-    });
+// table headings that will be placed in the "tenses" placeholder
+simple.click(function () { // onClick of the "simple" in the table heading
+    selectedTense.html(`<em class="selected-tense clicked">Simple</em>`);
+    console.log("Simple selected!");
+});
 
-    continuous.click(function(){
-        selectedTense.html(`<em class="selected-tense clicked">Continuous</em>`);
-        console.log("Continuous selected!");
-    });
+continuous.click(function () {
+    selectedTense.html(`<em class="selected-tense clicked">Continuous</em>`);
+    console.log("Continuous selected!");
+});
 
-    perfect.click(function(){
-        selectedTense.html(`<em class="selected-tense clicked">Perfect</em>`);
-        console.log("Perfect selected!");
+perfect.click(function () {
+    selectedTense.html(`<em class="selected-tense clicked">Perfect</em>`);
+    console.log("Perfect selected!");
 
-    });
+});
 
-    perfectContinuous.click(function(){
-        selectedTense.html(`<em class="selected-tense clicked">Perfect Continuous</em>`);
-    });
+perfectContinuous.click(function () {
+    selectedTense.html(`<em class="selected-tense clicked">Perfect Continuous</em>`);
+});
 
-    // table headings that will be placed in the "time" placeholder
-    past.click(function() {
-        selectedTime.html(`<em class="selected-time clicked">Past</em>`);
-    });
+// table headings that will be placed in the "time" placeholder
+past.click(function () {
+    selectedTime.html(`<em class="selected-time clicked">Past</em>`);
+});
 
-    present.click(function(){
-        selectedTime.html(`<em class="selected-time clicked">Present</em>`);
-    });
+present.click(function () {
+    selectedTime.html(`<em class="selected-time clicked">Present</em>`);
+});
 
-    future.click(function(){
-        selectedTime.html(`<em class="selected-time clicked">Future</em>`);
+future.click(function () {
+    selectedTime.html(`<em class="selected-time clicked">Future</em>`);
 
-    });
+});
 
+const object = {
+    a: 1,
+}
 
 /*
 selectedTense.replaceWith(`Yoyo`); // All event handlers will be removed after the first replaceWith() is executed?
