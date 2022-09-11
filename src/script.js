128d8c5383e1c11faa8a81a8c73ba7d1db924cc1
Shelly <shelly@Shellys-MacBook-Air.local>
2022-09-11 14:05:56 +0900

    feat: added sentences to data() as sentenceList array

--- src/script.js
+++ src/script.js
@@ -17,10 +17,59 @@ const tenseTable = new Vue({
                     tenseTime: 'future',
                     tenseType: 'continuous',
                 },
+                {
+                    sentence: `I will have played.`,
+                    tenseTime: 'future',
+                    tenseType: 'perfect',
+                },
+                {
+                    sentence: `I will have been playing.`,
+                    tenseTime: 'future',
+                    tenseType: 'perfect continuous',
+                },
                 // present tense
+                {
+                    sentence: `I play.`,
+                    tenseTime: 'present',
+                    tenseType: 'simple',
+                },
+                {
+                    sentence: `I am playing.`,
+                    tenseTime: 'present',
+                    tenseType: 'continuous',
+                },
+                {
+                    sentence: `I have played.`,
+                    tenseTime: 'present',
+                    tenseType: 'perfect',
+                },
+                {
+                    sentence: `I have been playing.`,
+                    tenseTime: 'present',
+                    tenseType: 'perfect continuous',
+                },
 
                 // past tense
-
+                {
+                    sentence: `I played.`,
+                    tenseTime: 'past',
+                    tenseType: 'simple',
+                },
+                {
+                    sentence: `I was playing.`,
+                    tenseTime: 'past',
+                    tenseType: 'continuous',
+                },
+                {
+                    sentence: `I had played.`,
+                    tenseTime: 'past',
+                    tenseType: 'perfect',
+                },
+                {
+                    sentence: `I had been playing.`,
+                    tenseTime: 'past',
+                    tenseType: 'perfect continuous',
+                },
             ]
         }
     }
