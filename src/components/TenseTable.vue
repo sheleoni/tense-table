dd34ee182ff5cd34ca158fbf794a5b39c9bf1d12
Sheleoni <sheleoni@gmail.com>
2022-10-16 17:45:02 +0900

    feat: reveal sentence when user clicks the correct cell and stop occlusion image from re-rendering

--- src/components/TenseTable.vue
+++ src/components/TenseTable.vue
@@ -44,11 +44,11 @@
       <h3 class="simple">Simple</h3>
       <!-- Past Simple -->
 
-      <p v-if="isOccluded.pastSimple === false" class="past-simple" data-tense-type="past-simple"
+      <p v-if="isOccluded['past-simple'] === false" class="past-simple" data-tense-type="past-simple"
         v-html="sentences.past.simple">
       </p>
       <p v-else class="past-simple" data-tense-type="past-simple" @click="checkAnswer()">
-        <img :src="require('../assets/img/clouds/' + randomOccluderImage())" :width="occluderImageWidth"
+        <img :src="require('../assets/img/clouds/' + cellImageURL['past-simple'])" :width="occluderImageWidth"
           data-tense-type="past-simple" />
       </p>
 
@@ -59,20 +59,20 @@
         <template v-else>Progressive</template>
       </h3>
 
-      <p v-if="isOccluded.pastContinuous === false" class="past-continuous" data-tense-type="past-continuous"
+      <p v-if="isOccluded['past-continuous'] === false" class="past-continuous" data-tense-type="past-continuous"
         v-html="sentences.past.continuous"></p>
       <p v-else class="past-continuous" data-tense-type="past-continuous" @click="checkAnswer()">
-        <img :src="require('../assets/img/clouds/' + randomOccluderImage())" :width="occluderImageWidth"
+        <img :src="require('../assets/img/clouds/' + cellImageURL['past-continuous'])" :width="occluderImageWidth"
           data-tense-type="past-continuous" />
       </p>
 
       <!-- Past Perfect -->
       <h3 class="perfect">Perfect</h3>
 
-      <p v-if="isOccluded.pastPerfect === false" class="past-perfect" data-tense-type="past-perfect"
+      <p v-if="isOccluded['past-perfect'] === false" class="past-perfect" data-tense-type="past-perfect"
         v-html="sentences.past.perfect"></p>
       <p v-else class="past-perfect" data-tense-type="past-perfect" @click="checkAnswer()">
-        <img :src="require('../assets/img/clouds/' + randomOccluderImage())" :width="occluderImageWidth"
+        <img :src="require('../assets/img/clouds/' + cellImageURL['past-perfect'])" :width="occluderImageWidth"
           data-tense-type="past-perfect" />
       </p>
 
@@ -83,10 +83,10 @@
       </h3>
 
       <!-- Past Perfect Continuous -->
-      <p v-if="isOccluded.pastPerfectContinuous === false" class="past-perfect-continuous"
-        data-tense-type="perfect-continuous" v-html="sentences.past.perfectContinuous"></p>
-      <p v-else class="past-perfect-continuous" data-tense-type="perfect-continuous" @click="checkAnswer()">
-        <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="perfect-continuous"
+      <p v-if="isOccluded['past-perfect-continuous'] === false" class="past-perfect-continuous"
+        data-tense-type="past-perfect-continuous" v-html="sentences.past.perfectContinuous"></p>
+      <p v-else class="past-perfect-continuous" data-tense-type="past-perfect-continuous" @click="checkAnswer()">
+        <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="past-perfect-continuous"
           :width="occluderImageWidth" />
       </p>
 
@@ -96,7 +96,7 @@
 
       <!-- Present Simple -->
       <h3 class="repeated-heading">Simple</h3>
-      <p v-if="isOccluded.presentSimple === false" class="present-simple" data-tense-type="present-simple"
+      <p v-if="isOccluded['present-simple'] === false" class="present-simple" data-tense-type="present-simple"
         v-html="sentences.present.simple"></p>
       <p v-else class="present-simple" data-tense-type="present-simple" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="present-simple"
@@ -106,8 +106,8 @@
 
       <!-- Present Continuous -->
       <h3 class="repeated-heading">Continuous/ Progressive</h3>
-      <p v-if="isOccluded.presentContinuous === false" class="present-continuous" data-tense-type="present-continuous"
-        v-html="sentences.present.continuous"></p>
+      <p v-if="isOccluded['present-continuous'] === false" class="present-continuous"
+        data-tense-type="present-continuous" v-html="sentences.present.continuous"></p>
       <p v-else class="present-continuous" data-tense-type="present-continuous" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="present-continuous"
           :width="occluderImageWidth" />
@@ -117,7 +117,7 @@
       <!-- Present Perfect -->
       <h3 class="repeated-heading">Perfect</h3>
 
-      <p v-if="isOccluded.presentPerfect === false" class="present-perfect" data-tense-type="present-perfect"
+      <p v-if="isOccluded['present-perfect'] === false" class="present-perfect" data-tense-type="present-perfect"
         v-html="sentences.present.perfect"></p>
       <p v-else class="present-perfect" data-tense-type="present-perfect" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="present-perfect"
@@ -128,7 +128,7 @@
       <!-- Present Perfect Continuous -->
       <h3 class="repeated-heading">Perfect Continuous/ Perfect Progressive</h3>
 
-      <p v-if="isOccluded.presentPerfectContinuous === false" class="present-perfect-continuous"
+      <p v-if="isOccluded['present-perfect-continuous'] === false" class="present-perfect-continuous"
         data-tense-type="present-perfect-continuous" v-html="sentences.present.perfectContinuous">
       </p>
       <p v-else class="present-perfect-continuous" data-tense-type="present-perfect-continuous" @click="checkAnswer()">
@@ -144,7 +144,7 @@
       <!-- Future Simple -->
       <h3 class="repeated-heading">Simple</h3>
 
-      <p v-if="isOccluded.futureSimple === false" class="future-simple" data-tense-type="future-simple"
+      <p v-if="isOccluded['future-simple'] === false" class="future-simple" data-tense-type="future-simple"
         v-html="sentences.future.simple"></p>
       <p v-else class=" future-simple" data-tense-type="future-simple" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" :width="occluderImageWidth"
@@ -154,7 +154,7 @@
 
       <!-- Future Continuous -->
       <h3 class="repeated-heading">Continuous/ Progressive</h3>
-      <p v-if="isOccluded.futureContinuous === false" class="future-continuous" data-tense-type="future-continuous"
+      <p v-if="isOccluded['future-continuous'] === false" class="future-continuous" data-tense-type="future-continuous"
         v-html="sentences.future.continuous">
       </p>
       <p v-else class="future-continuous" data-tense-type="future-continuous" @click="checkAnswer()">
@@ -165,7 +165,7 @@
 
       <!-- Future Perfect -->
       <h3 class="repeated-heading">Perfect</h3>
-      <p v-if="isOccluded.futurePerfect === false" class="future-perfect" data-tense-type="future-perfect"
+      <p v-if="isOccluded['future-perfect'] === false" class="future-perfect" data-tense-type="future-perfect"
         v-html="sentences.future.perfect"></p>
       <p v-else class="future-perfect" data-tense-type="future-perfect" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" data-tense-type="future-perfect"
@@ -175,7 +175,7 @@
 
       <!-- Future Perfect Continuous -->
       <h3 class="repeated-heading">Perfect Continuous/ Perfect Progressive</h3>
-      <p v-if="isOccluded.futurePerfectContinuous === false" class="future-perfect-continuous"
+      <p v-if="isOccluded['future-perfect-continuous'] === false" class="future-perfect-continuous"
         data-tense-type="future-perfect-continuous" v-html="sentences.future.perfectContinuous"></p>
       <p v-else class="future-perfect-continuous" data-tense-type="future-perfect-continuous" @click="checkAnswer()">
         <img :src="require('../assets/img/clouds/' + randomOccluderImage())" :width="occluderImageWidth"
@@ -233,20 +233,33 @@ export default {
       // occlusion for each cell
       // isOccluded: false,
       isOccluded: {
-        pastSimple: false,
-        pastContinuous: false,
-        pastPerfect: false,
-        pastPerfectContinuous: false,
-        presentSimple: false,
-        presentContinuous: false,
-        presentPerfect: false,
-        presentPerfectContinuous: false,
-        futureSimple: false,
-        futureContinuous: false,
-        futurePerfect: false,
-        futurePerfectContinuous: false,
+        'past-simple': false,
+        'past-continuous': false,
+        'past-perfect': false,
+        'past-perfect-continuous': false,
+        'present-simple': false,
+        'present-continuous': false,
+        'present-perfect': false,
+        'present-perfect-continuous': false,
+        'future-simple': false,
+        'future-continuous': false,
+        'future-perfect': false,
+        'future-perfect-continuous': false,
       },
-
+      cellImageURL: {
+        'past-simple': '',
+        'past-continuous': '',
+        'past-perfect': '',
+        'past-perfect-continuous': '',
+        'present-simple': '',
+        'present-continuous': '',
+        'present-perfect': '',
+        'present-perfect-continuous': '',
+        'future-simple': '',
+        'future-continuous': '',
+        'future-perfect': '',
+        'future-perfect-continuous': '',
+      },
       shuffledSentenceList: [],
     }
   },
@@ -331,11 +344,19 @@ export default {
 
       this.getCellTenseType();
       if (this.selectedTenseType === this.answerTenseType) {
+        console.log(this.selectedTenseType);
+        console.log(this.answerTenseType);
         console.log("correct!");
         this.score = this.score + 1;
+        // console.log(this.isOccluded);
         // next: reveal sentence of that cell
-        // const sentenceObject = JSON.parse(JSON.stringify(this.isOccluded));
-        // console.log(sentenceObject);
+        const sentenceObject = JSON.parse(JSON.stringify(this.isOccluded));
+        console.log(sentenceObject);
+
+        console.log(this.isOccluded);
+
+        console.log(this.isOccluded === sentenceObject);
+
         // this.sentenceObject[this.selectedTenseType] = false;
         // console.log(this.sentenceObject[this.selectedTenseType]);
         console.log(this.isOccluded.futureContinuous);
@@ -363,6 +384,13 @@ export default {
       return randomImage;
     }
 
+  },
+  mounted() {
+    for (var item in this.cellImageURL) {
+      const images = this.occluderImages;
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      this.cellImageURL[item] = randomImage;
+    }
   },
   computed: {
     sentenceList: function () {
