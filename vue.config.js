d5fc2adb602a768434d55a1200b9b49aaf181d45
Sheleoni <sheleoni@gmail.com>
2022-12-26 16:02:21 +0800

    added comments for router

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: './'
+  publicPath: '/', // to fix routing issue: either use '/' as publicpath, or comment this line out
 })
